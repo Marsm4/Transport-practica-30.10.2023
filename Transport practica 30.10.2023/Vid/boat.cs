56b8a73f29da5f7cb969841703f93ad8e283cda1
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-10-30 18:57:32 +0300

    class boat

---
+++ Transport practica 30.10.2023/Vid/boat.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Reflection;
+using TransportManagement;
+
+class Boat : Transport
+{
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Boat:");
+        Console.WriteLine("Model: " + Model);
+        Console.WriteLine("Color: " + Color);
+        Console.WriteLine("Speed: " + Speed);
+        Console.WriteLine("Is Rented: " + IsRented);
+        Console.WriteLine();
+    }
+}
