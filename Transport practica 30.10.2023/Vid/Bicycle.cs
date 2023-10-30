238ff68ae67855936092ea98e502001f1eef3a18
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-10-30 19:00:26 +0300

    class bicycle

---
+++ Transport practica 30.10.2023/Vid/Bicycle.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Reflection;
+using TransportManagement;
+
+class Bicycle : Transport
+{
+    public int NumberOfWheels { get; set; }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Bicycle:");
+        Console.WriteLine("Model: " + Model);
+        Console.WriteLine("Color: " + Color);
+        Console.WriteLine("Speed: " + Speed);
+        Console.WriteLine("Is Rented: " + IsRented);
+        Console.WriteLine("Number of Wheels: " + NumberOfWheels);
+        Console.WriteLine();
+    }
+}
