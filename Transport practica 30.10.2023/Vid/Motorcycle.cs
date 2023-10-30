a1beb1a795868a41431778377f4a31eb179ff4aa
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-10-30 19:04:26 +0300

    +2classa

---
+++ Transport practica 30.10.2023/Vid/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Reflection;
+using TransportManagement;
+
+class Motorcycle : Transport
+{
+    public string LicenseCategory { get; set; }
+    public bool HasMotor { get; set; }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Motorcycle:");
+        Console.WriteLine("Model: " + Model);
+        Console.WriteLine("Color: " + Color);
+        Console.WriteLine("Speed: " + Speed);
+        Console.WriteLine("Is Rented: " + IsRented);
+        Console.WriteLine("License Category: " + LicenseCategory);
+        Console.WriteLine("Has Motor: " + HasMotor);
+        Console.WriteLine();
+    }
+}
---
+++ Transport practica 30.10.2023/Vid/scooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using System.Reflection;
+using TransportManagement;
+
+class Scooter : Transport
+{
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Scooter:");
+        Console.WriteLine("Model: " + Model);
+        Console.WriteLine("Color: " + Color);
+        Console.WriteLine("Speed: " + Speed);
+        Console.WriteLine("Is Rented: " + IsRented);
+        Console.WriteLine();
+    }
+}
