1feede563591ed593d6c29e8540e56784da87db6
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-10-30 18:58:52 +0300

    class car

---
+++ Transport practica 30.10.2023/Vid/car.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using System.Reflection;
+using TransportManagement;
+
+class Car : Transport
+{
+    public string Category { get; set; }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Car:");
+        Console.WriteLine("Model: " + Model);
+        Console.WriteLine("Color: " + Color);
+        Console.WriteLine("Speed: " + Speed);
+        Console.WriteLine("Is Rented: " + IsRented);
+        Console.WriteLine("Category: " + Category);
+        Console.WriteLine();
+    }
+}
