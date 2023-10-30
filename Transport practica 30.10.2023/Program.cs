806b60c6be2620ae95a1b8f4777203b86a49b63e
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-10-30 18:54:26 +0300

    Добавьте файлы проекта.

---
+++ Transport practica 30.10.2023/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TransportManagement
+{
+    // Базовый класс для транспорта
+    abstract class Transport
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+        public bool IsRented { get; set; }
+
+        public abstract void PrintInfo();
+    }
+}
+
+   
